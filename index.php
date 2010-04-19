16290ae0f518fc56a26ec4ccdc25d8692efd00ac
Infinity1804 <Infinity1804@gmail.com>
2010-04-19 18:38:16 +0000

    Ordnerstruktur geändert

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?PHP
+require_once("ts3phpfw/ts3admin.class.php");
+
+$ts3 = new Teamspeak3();
+$ts3->list_all_channels();
+?>
+
+</body>
+</html>
